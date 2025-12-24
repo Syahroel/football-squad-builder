2535d4a5bf19d171402d03d756bdfa8932637f8f
Syahrul Muchtarom <muchtaromsyahrul@gmail.com>
2025-12-24 10:50:11 +0700

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,7 +4,6 @@ export default {
   schema: './db/schema.ts',
   out: './drizzle',
   dialect: 'sqlite',
-  driver: 'turso',
   dbCredentials: {
     url: process.env.TURSO_DATABASE_URL!,
     authToken: process.env.TURSO_AUTH_TOKEN,
