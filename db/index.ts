2f54b0665356c7a725e477df4238658b51bbc908
Syahrul Muchtarom <muchtaromsyahrul@gmail.com>
2025-12-24 10:52:19 +0700

    Update index.ts

--- db/index.ts
+++ db/index.ts
@@ -5,6 +5,7 @@ import * as schema from './schema';
 const client = createClient({
   url: process.env.TURSO_DATABASE_URL!,
   authToken: process.env.TURSO_AUTH_TOKEN,
+  intMode: 'number',
 });
 
 export const db = drizzle(client, { schema });
